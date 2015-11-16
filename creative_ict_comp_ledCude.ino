1644ef75202cd94c38299bb6b4a837cf3b005c4a
Cho Kwanghee <chokh1443@gmail.com>
2015-11-17 02:05:40 +0900

    comment added

--- creative_ict_comp_ledCude.ino
+++ creative_ict_comp_ledCude.ino
@@ -1,4 +1,10 @@
-class  Cube {
+/*
+ * 2015-11-17 Cho Kwanghee ( https://github.com/chokh1443 )
+ * Term Project of Creative ICT Compution class at CAU, South Korea
+ * Class to controll 4*4*4 LED cube using Arduino Uno 
+ */
+ 
+class  Cube {//class to controll LED CUBE
 private:
   int pinSize = 16;
   int layerSize = 4;
@@ -6,17 +12,19 @@ private:
   byte layer[4] = {A2,A3,A4,A5};
   byte *status;
 public:
-  void offById(int);
-  //void offByLayer(int);
-  void offAll();
-  void onById(int);
-  //void onByLayer(int);
-  void onAll();
-  void timer0Init();
-  void init();
+  void offById(int);           //turn off single led indexed by id
+  //void offByLayer(int);      //xTODO : turn off single layer indexed by layer id
+  void offAll();               //turn off all led
+  void onById(int);            //turn on single 
+  //void onByLayer(int);       //xTODO : turn on single layer indexed by layer id
+  void onAll();                //turn on all led
+  void timer0Init();           //timer0 interrupt initialize with frequency of 240hz
+  void init();                 //initialize arduino uno pin setting
+  void showByLayer(int);       //display led of single layer that should be called at timer0 interrupt
+  
   int getSizeOfCube(){ return (this->pinSize * this->layerSize); };
   int getSizeOfLayer(){ return this->layerSize; };
-  void showByLayer(int);
+  
 };
 void Cube::init(){
   for(int i=0;i<this->pinSize;i++){
@@ -45,9 +53,9 @@ void Cube::onAll(){
   }
 }
 void Cube::timer0Init(){
-  //240Hz
+  //setting timer0 interrupt with 240Hz
   cli();
-  //CTC mode, 분주비 1024
+  //CTC mode, clkio 1024
   //datasheet pg106
   TCCR0A = 0x02;// (1<<WGM01)
   TCCR0B = 0x05;// (1<<CS01) | (1<<CS00)
@@ -77,7 +85,7 @@ void Cube::showByLayer(int num){
 }
 
 Cube cube;
-int ms4 = 0;
+int ms4 = 0;//increase this variable every 240hz and cleared at 240
 void setup() {
   cube.init();
   cube.timer0Init();
@@ -92,11 +100,10 @@ void loop() {
 }
 
 ISR(TIMER0_COMPA_vect){
-  //Interrupt Subrutine
+  //timer0 interrupt subrutine
   static int counter = 0;
-  ms4 = ++ms4%250;
+  ms4 = ++ms4%240;
   counter = ++counter%cube.getSizeOfLayer();
-  cube.showByLayer(counter);
-  
+  cube.showByLayer(counter);  
 }
 
