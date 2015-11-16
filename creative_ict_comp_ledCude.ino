a90a2dd00c1468373cce8cd92aacf335bddca1c3
Cho Kwanghee <chokh1443@gmail.com>
2015-11-17 01:53:58 +0900

    timer0 interrupt freq = 240hz

--- creative_ict_comp_ledCude.ino
+++ creative_ict_comp_ledCude.ino
@@ -5,21 +5,27 @@ private:
   byte pin[16] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,A0,A1};
   byte layer[4] = {A2,A3,A4,A5};
   byte *status;
-  void initCube();
 public:
   void offById(int);
+  //void offByLayer(int);
   void offAll();
-  void timer1Init();
-  Cube(){ initCube(); }
+  void onById(int);
+  //void onByLayer(int);
+  void onAll();
+  void timer0Init();
+  void init();
+  int getSizeOfCube(){ return (this->pinSize * this->layerSize); };
+  int getSizeOfLayer(){ return this->layerSize; };
+  void showByLayer(int);
 };
-void Cube::initCube(){
+void Cube::init(){
   for(int i=0;i<this->pinSize;i++){
     pinMode(this->pin[i],OUTPUT);
   }
   for(int i=0;i<this->layerSize;i++){
     pinMode(this->layer[i],OUTPUT);
   }
-  this->status = new byte[pinSize * layerSize];
+  this->status = new byte[this->pinSize * this->layerSize];
   this->offAll();
 }
 void Cube::offById(int id){
@@ -30,14 +36,67 @@ void Cube::offAll(){
     this->offById(i);
   }
 }
-void Cube::timer1Init(){
-  TCCR1A = 0x00;
-  TCCR3A = 0x00;
+void Cube::onById(int id){
+  this->status[id] = 1;
+}
+void Cube::onAll(){
+  for(int i=0;i< (this->pinSize * this->layerSize); i++){
+    this->onById(i);
+  }
+}
+void Cube::timer0Init(){
+  //240Hz
+  cli();
+  //CTC mode, 분주비 1024
+  //datasheet pg106
+  TCCR0A = 0x02;// (1<<WGM01)
+  TCCR0B = 0x05;// (1<<CS01) | (1<<CS00)
+  TCNT0 = 0x00;
+  OCR0A = 0x40;//64
+  TIMSK0 = 0x02;// (1<<OCIE0A);
+  sei();
+}
+void Cube::showByLayer(int num){
+  for(int i=0;i<this->pinSize;i++){
+    if(this->status[(num*16+i)] == 1) {
+      
+      digitalWrite(this->pin[i],HIGH);  
+    }
+    else {  
+      digitalWrite(this->pin[i],LOW);
+    }
+  }
+  for(int i=0;i<this->layerSize;i++){
+    if(i == num){
+      digitalWrite(this->layer[i],HIGH);
+    }
+    else {
+      digitalWrite(this->layer[i],LOW);
+    }
+  }
 }
 
+Cube cube;
+int ms4 = 0;
 void setup() {
-  Cube cube;
+  cube.init();
+  cube.timer0Init();
 }
 
 void loop() {
+  if(ms4 < 120){
+    cube.onAll();
+  }else{
+    cube.offAll();
+  }
 }
+
+ISR(TIMER0_COMPA_vect){
+  //Interrupt Subrutine
+  static int counter = 0;
+  ms4 = ++ms4%250;
+  counter = ++counter%cube.getSizeOfLayer();
+  cube.showByLayer(counter);
+  
+}
+
